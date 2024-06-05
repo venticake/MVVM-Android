8372a7312fc9a84b8b081af853a933c5fcac008d
wondae-code <noin1996@naver.com>
2024-06-05 16:46:53 +0900

    Todo -> TodoRecord로 이름 변경 후 realm schema 버전 증가

--- app/src/main/java/com/example/mvvm_android/BaseApplication.java
+++ app/src/main/java/com/example/mvvm_android/BaseApplication.java
@@ -13,11 +13,14 @@ public class BaseApplication extends Application {
         // Initialize Realm
         Realm.init(this);
         RealmConfiguration config = new RealmConfiguration.Builder()
-                .name("todo.realm")
-                .schemaVersion(1)
+                .name("todoRecord.realm")
+                .schemaVersion(2)
                 // Todo : UI Thread가 아닌, background에서 동작하도록 수정하기.
+                // realm 전용 thread를 만들어서 처리하기
                 .allowWritesOnUiThread(true)
                 .build();
+
+
 
         Realm.setDefaultConfiguration(config);
     }
